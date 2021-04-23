044b9e54ec8d4bc3632272c8d1f825ba9f854020
Jxq7 <79334626+Jxq7@users.noreply.github.com>
2021-04-23 14:54:01 +0200

    Create Ragdoll Engine [ASX].lua

---
+++ Ragdoll Engine [ASX].lua
@@ -0,0 +1,2 @@
+--- Ok I'm not the creator of this script but its for the crash / lag script
+loadstring(game:HttpGet(('https://gitlab.com/Tsuniox/lua-stuff/-/raw/master/RagdollEngineGUI.lua'),true))()
