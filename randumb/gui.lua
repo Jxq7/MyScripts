aaa8f70254ce5c2bfcd706b31fa2ea83958b2d41
Jxq7 <79334626+Jxq7@users.noreply.github.com>
2022-01-12 19:12:51 +0100

    updated services and added
    Working on it should be out in a few days unless i face some issues.

--- randumb/gui.lua
+++ randumb/gui.lua
@@ -1,1 +1,26 @@
-
+-- DH AUTOFARM
+-- services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TeleportService = game:GetService("TeleportService")
+local TweenService = game:GetService("TweenService")
+local StarterGui = game:GetService("StarterGui")
+local Lighting = game:GetService("Lighting")
+local Player = game.Players.LocalPlayer
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TeleportService = game:GetService("TeleportService")
+local TweenService = game:GetService("TweenService")
+local StarterGui = game:GetService("StarterGui")
+local Lighting = game:GetService("Lighting")
+local Player = game.Players.LocalPlayer
+local HttpService = game:GetService("HttpService")
+------
+------ 
+print("DH Autofarm has succesfully been injected into your game, sending notification to client!")
+StarterGui:SetCore("SendNotification", {
+		Title = "Message from me";
+		Text = "DH Autofarm GUI Activated"
+	})
+end)
+---- GUI
+----
